6640a62c9103f27cf26c2eec6b023de8f854f5a9
IoanaTon <95285246+IoanaTon@users.noreply.github.com>
2022-10-16 19:03:16 +0300

    tasks 1-4

---
+++ lab1/src/task2/Student.java
@@ -0,0 +1,21 @@
+package lab1.task2;
+
+public class Student {
+    private String name;
+    private double grade;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+}
